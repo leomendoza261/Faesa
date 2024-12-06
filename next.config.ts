3eace574c9f3801824259ec89452f4ab9735bc08
leomendoza261 <pedrolmendoza031297@gmail.com>
2024-12-06 10:25:36 -0300

    correccion en la cabeceras de netlify

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  experimental: {
+    serverActions: {
+      strictOrigin: false,
+    },
+  },
+};
+
 export default nextConfig;
